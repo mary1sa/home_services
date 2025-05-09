714304f969aa3f2fdfbacb659028b5447706de0d
labroudimariam <labroudimariam@gmail.com>
2025-05-09 20:29:27 +0100

    Add initial controllers, models, and migrations for home services backend

--- home_services_backend/database/migrations/0001_01_01_000000_create_users_table.php
+++ home_services_backend/database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,12 +13,14 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('first_name');
+            $table->string('last_name');
             $table->string('email')->unique();
-            $table->enum('role', ['user', 'tasker', 'admin'])->default('user');
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->rememberToken();
+            $table->string('phone');
+            $table->string('address')->nullable();
+            $table->boolean('is_online')->default(false);
+            $table->enum('role', ['user', 'tasker', 'admin'])->default('user');
             $table->timestamps();
         });
 
---
+++ home_services_backend/database/migrations/2025_05_09_183228_create_taskers_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('taskers', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->string('country')->nullable();
+            $table->string('city');
+            $table->string('cin');
+            $table->string('certificate_police');
+            $table->date('certificate_police_date');
+            $table->text('bio')->nullable();
+            $table->integer('experience')->nullable();
+            $table->string('photo')->nullable();
+            $table->enum('status', ['pending', 'approved', 'rejected'])->default('pending');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('taskers');
+    }
+};
